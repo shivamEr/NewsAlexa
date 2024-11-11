2f27f7a1e4370127bb5391ffecddd5d2778fa6e1
shivam kumar <shivamksbabu504@gmail.com>
2024-11-11 18:41:36 +0530

    Infinite Scroll Added

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import NewsItem from './NewsItem'
 import Loading from './Loading';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 export default class News extends Component {
     static defaultProps = {
@@ -15,15 +17,16 @@ export default class News extends Component {
         category: PropTypes.string
     }
 
-    capitalizeFirstLetter = (string)=>{
+    capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
     constructor(props) {
         super(props);
         this.state = {
             articles: [],
-            loading: false,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
         document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsAlexa`
     }
@@ -53,19 +56,40 @@ export default class News extends Component {
         this.update();
     }
 
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 });
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=85c94ee44c1d40fd9c619d60558071ce&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parseData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles),
+            totalResults: parseData.totalResults
+        });
+    };
+
     render() {
         return (
             <div className='container my3'>
                 <h1 className='my-3 text-center'>NewsAlexa - Top Headlines On {this.capitalizeFirstLetter(this.props.category)}</h1>
                 {this.state.loading && <Loading />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://t4.ftcdn.net/jpg/06/95/90/53/360_F_695905385_58pzoib9wHFl4lK2YEVDbLA5f2B3z5iu.jpg"} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Loading />}
+                >
+                    <div className="container">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-4" key={element.url}>
+                                    <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://t4.ftcdn.net/jpg/06/95/90/53/360_F_695905385_58pzoib9wHFl4lK2YEVDbLA5f2B3z5iu.jpg"} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>
+                            })
+                            }
                         </div>
-                    })
-                    }
-                </div>
+                    </div>
+                </InfiniteScroll>
                 <div className="container row">
                     <button type="button" disabled={this.state.page <= 1} className="my-3 col-md-1 btn btn-dark" onClick={this.handlePreClick}>&larr; Preview</button>
                     <button type="button" className="my-3 col-md-1 offset-10 btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
